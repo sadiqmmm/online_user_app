795a9ceac70fa887cec9cb3a2e1f500327df1e73
Mohammed Sadiq <sadiqmmm@gmail.com>
2020-02-11 19:56:01 +0530

    Keep track of users who are online with a database column

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :set_last_seen_at, if: :user_signed_in?
+  
+  def set_last_seen_at
+    current_user.touch(:last_seen_at)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  scope :online, ->{ where("last_seen_at > ?", 15.minutes.ago) }       
 end
